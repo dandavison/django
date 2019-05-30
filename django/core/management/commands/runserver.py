bf9d454f954816ef3241644d8068c2011a064bef
Dan Davison <dandavison7@gmail.com>
2019-05-30 12:48:19 +0300

    Revert "Try launch_new_instance instead of embed_kernel"
    This reverts commit c1f16ed6b8c4bc0ff986c484e45e37d27f88c779.

--- django/core/management/commands/runserver.py
+++ django/core/management/commands/runserver.py
@@ -175,7 +175,7 @@ BaseRunserverCommand = Command
 from unittest import mock
 from threading import Thread
 
-from ipykernel.kernelapp import launch_new_instance
+from ipykernel.embed import embed_kernel
 
 
 class JupyterPythonKernelThread(Thread):
@@ -184,4 +184,4 @@ class JupyterPythonKernelThread(Thread):
         # ValueError: signal only works in main thread
         # https://github.com/ipython/ipython/issues/4032
         with mock.patch('signal.signal'):
-            return launch_new_instance(argv=[])
+            return embed_kernel(argv=[])
